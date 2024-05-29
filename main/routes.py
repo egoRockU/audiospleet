71e3ce24844e67c8b79b7c95b26a78546d3f4bb8
egoRockU <lagutan.gerwin.bscs2021@gmail.com>
2024-05-29 19:56:11 +0800

    no file bug fix

--- main/routes.py
+++ main/routes.py
@@ -10,19 +10,25 @@ def hello():
 @main.route('/')
 def home():
     session['file_valid'] = False  
-    return render_template("home.html", file_valid=False)
+    return render_template("home.html")
 
 @main.route('/checkfile', methods=['POST'])
 def checkfile():
     types = ['mp3', 'wav', 'flac', 'm4a']
-    audio = request.files['audio']
-    audio_ext = audio.filename.split('.')[-1]
 
+    try:
+        audio = request.files['audio']
+    except Exception as err:
+         print("Error" + repr(err))
+         return render_template('validate_results/nofile.html')
+    
+    audio_ext = audio.filename.split('.')[-1]
     for audio_type in types:
         if audio_ext == audio_type:
             session['file_valid'] = True
-            return render_template('validate_results/valid.html', file_valid=True)
-    return render_template('validate_results/invalid.html', file_valid=False)
+            return render_template('validate_results/valid.html')
+    return render_template('validate_results/invalid.html')
+
 
 @main.route('/spleet', methods=['POST'])
 def spleet():
